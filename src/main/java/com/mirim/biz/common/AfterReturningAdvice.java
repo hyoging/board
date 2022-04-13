ac567a803b8abf1aaa34282862db8d56b9c1e5f3
hyoging <w2030@e-mirim.hs.kr>
2022-04-13 09:22:40 +0900

    after-returning

--- src/main/java/com/mirim/biz/common/AfterReturningAdvice.java
+++ src/main/java/com/mirim/biz/common/AfterReturningAdvice.java
@@ -1,7 +1,18 @@
 package com.mirim.biz.common;
 
+import com.mirim.biz.user.UserVO;
+import org.aspectj.lang.JoinPoint;
+
 public class AfterReturningAdvice {
-    public void AfterReturningLog(){
-        System.out.println("[사후처리] 비즈니스 로직 수행 후 동작");
+    public void AfterReturningLog(JoinPoint jp, Object returnObj){
+        String method = jp.getSignature().getName();
+
+        if(returnObj instanceof UserVO){    //객체 타입 판별
+            UserVO user = (UserVO) returnObj;
+            if(user.getRole().equals("Admin")){
+                System.out.println(user.getName() + "로그인(Admin)");
+            }
+        }
+        System.out.println("[사후처리]" + method + "() 메소드 리턴 값: " + returnObj.toString());
     }
 }
